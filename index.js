d3c73b25996592cf825b1dab39a46fdb6c7172e1
delusional51 <BenjaminZanelli51@gmail.com>
2023-10-19 13:14:13 -0400

    html done, doing func for insert
    use image u have lol

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 DOMSelectors = {
     form: document.querySelector("#form"),
-    itemname: document.querySelector(".item"),
+    itemname: document.querySelector("#item-name"),
+    imagelink: document.querySelector("#image-link"),
     h2s: document.querySelectorAll("h2"),
     remove: document.querySelector(".remove"),
+    imtem: document.querySelector(".item"),
 };
+function addthing() {
+    document.querySelector(".item").insertAdjacentHTML(
+        "beforeend",
+        `<div class="item">
+         <h2 id="name"></h2>
+        <img src="" alt="user entered image">
+        <button class="remove" >REMOVE</button> ]
+    </div>`
+    );
+}
 DOMSelectors.form.addEventListener("submit", function(event) {
     event.preventDefault();
-    DOMSelectors.h2s.forEach(
-        (item) => (item.textContent = DOMSelectors.itemname.value)
-    );
 }); 
 
 /* function getrid() {
    DOMSelectors.h2s.forEach((h2) => (h2.textContent = null) ); }
  */
-DOMSelectors.remove.addEventListener("click", function(){
+/* DOMSelectors.remove.addEventListener("click", function(){
     DOMSelectors.h2s.forEach((h2) => (h2.textContent = null))
-})
+}) */
